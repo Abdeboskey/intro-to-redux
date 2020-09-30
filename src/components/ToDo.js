6e5cecf8c4c9fad6c769d82c6bd5eb7cad8d17c8
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-30 17:34:29 -0600

    Connect to store and add dispatch togglecomplete action

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { toggleComplete } from '../actions';
 
-const ToDo = ({ id, text, completed }) => {
+const ToDo = ({ id, text, completed, toggleComplete }) => {
   return (
-    <li>{text}</li>
+    <li 
+      className={completed ? 'completed' : 'not-completed'}
+      onClick={() => toggleComplete(id)}
+    >{text}</li>
   )
 }
 
-export default ToDo
+const mapDispatchToProps = dispatch => ({
+  toggleComplete: id => dispatch(toggleComplete(id)),
+})
+
+export default connect(null, mapDispatchToProps)(ToDo)
